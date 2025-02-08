b70d89187c0a8aa29ffcd10cd4328d9f64a7a74d
MuhmedAshraf <100963016+MuhmedAshraf@users.noreply.github.com>
2025-02-08 16:01:44 +0200

    arch&splash

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:new_bookly/Features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashView(),
+    );
+  }
+}
