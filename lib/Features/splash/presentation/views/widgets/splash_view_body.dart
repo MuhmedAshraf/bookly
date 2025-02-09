52c1921b716e51cea06a5ab87deb53e02d2443bc
MuhmedAshraf <100963016+MuhmedAshraf@users.noreply.github.com>
2025-02-09 17:37:02 +0200

    splashView ui done

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:new_bookly/Core/utils/assets.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AppAssets.logo),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:new_bookly/Features/splash/presentation/views/splash_view.dart';
+import 'package:new_bookly/constants.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -10,8 +11,10 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashView(),
+    return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: kPrimaryColor),
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
     );
   }
 }
